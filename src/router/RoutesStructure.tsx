918a3452b40affaa4fb5b8d49d6a834764c8d72d
horhoj <horhoj@yandex.ru>
2021-05-26 18:53:48 +0500

    refactor RoutesStructure.tsx

--- src/router/RoutesStructure.tsx
+++ src/router/RoutesStructure.tsx
@@ -5,43 +5,50 @@ import { authSelectors } from '../store/auth';
 import { PrivateLayout } from '../layouts/PrivateLayout';
 import { PublicLayout } from '../layouts/PublicLayout';
 import { getPathByName, routes } from './routes';
+import { RouteItem } from './types';
 
 export const RoutesStructure: React.FC = () => {
   const userIsAuthenticated = useSelector(authSelectors.getIsAuthenticated);
+
+  const getRouteAction = (route: RouteItem) => {
+    //вся конфигурация роутов задана в router/routes
+    //если  для компонента  задано showAlways то немедленно выводим
+    if (route.always) {
+      return <route.component />;
+    }
+    //проверяем предназначен ли компонент для работы, если мы вошли в систему
+    if (route.private) {
+      //если да, то проверяем залогинены ли мы
+      //и если то показываем его
+      //иначе делаем редирект на страницу логина
+      if (userIsAuthenticated) {
+        return (
+          <PrivateLayout>
+            <route.component />
+          </PrivateLayout>
+        );
+      }
+      return <Redirect to={getPathByName('login')} />;
+    }
+    //если нет, то то проверяем залогинены ли мы
+    //и если да то делаем редирект на главную страницу
+    //иначе показываем компонент
+    if (userIsAuthenticated) {
+      return <Redirect to={getPathByName('home')} />;
+    }
+    return (
+      <PublicLayout>
+        <route.component />
+      </PublicLayout>
+    );
+  };
 
   return (
     <BrowserRouter>
       <Switch>
         {routes.map((route) => (
           <Route path={route.path} exact={route.exact} key={route.name}>
-            {
-              //вся конфигурация роутов задана в router/routes
-              //если  для компонента  задано showAlways то немедленно выводим
-              route.always ? (
-                <route.component />
-              ) : //проверяем предназначен ли компонент для работы, если мы вошли в систему
-              route.private ? (
-                //если да, то проверяем залогинены ли мы
-                //и если то показываем его
-                //иначе делаем редирект на страницу логина
-                userIsAuthenticated ? (
-                  <PrivateLayout>
-                    <route.component />
-                  </PrivateLayout>
-                ) : (
-                  <Redirect to={getPathByName('login')} />
-                )
-              ) : //если нет, то то проверяем залогинены ли мы
-              //и если да то делаем редирект на главную страницу
-              //иначе показываем компонент
-              userIsAuthenticated ? (
-                <Redirect to={getPathByName('home')} />
-              ) : (
-                <PublicLayout>
-                  <route.component />
-                </PublicLayout>
-              )
-            }
+            {getRouteAction(route)}
           </Route>
         ))}
       </Switch>
